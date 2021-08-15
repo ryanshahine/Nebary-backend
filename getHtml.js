a8ce998f5344724c55f9a86077b49bc4a765d261
Ryan <contact@ryanshahine.me>
2021-08-16 00:42:48 +0200

    Update getHtml.js

--- getHtml.js
+++ getHtml.js
@@ -27,9 +27,8 @@ function getHtml(items) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nebary - Decentralized Media Sharing</title>
-    <link rel="stylesheet" href="https://ipfs.infura.io/ipfs/QmQTtPLDdpyLsNEJ2heySBfYmeJYV3vjy4UsMhonTsMehZ">
+    <link rel="stylesheet" href="https://bafybeia7shtufhpszi6grhkrm3kunueww2rjmarzzne32qi3ch5pqkjweq.ipfs.infura-ipfs.io/">
     <link rel="stylesheet" href="https://cdn.plyr.io/3.6.8/plyr.css" />
-    <script src="https://ipfs.infura.io/ipfs/QmRf5RdSiQv1QvJCMg6CghCiTFfmM46qSTHhWP8cwt1uFi"></script>
   </head>
   <body>
     <!-- This example requires Tailwind CSS v2.0+ -->
@@ -134,6 +133,7 @@ function getHtml(items) {
     </div>
   
     <script src="https://cdn.plyr.io/3.6.8/plyr.js"></script>
+    <script src="https://bafybeibrjn3dev52usjmtxizvhrsbjii2kkgasoj6p55eavm2gjriope2u.ipfs.infura-ipfs.io/"></script>
   </body>
   </html>`
 }
